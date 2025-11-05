859aa3eb9925213cdd500bc490aaaea558485b64
Kurasao <jamerolenor@gmail.com>
2025-11-05 08:39:20 +0800

    refactor: adjust styling in StackBox component for consistency and improved layout

--- src/components/ui/StackBox.tsx
+++ src/components/ui/StackBox.tsx
@@ -5,9 +5,9 @@ type StackItem = {
 
 const StackBox = ({ stack }: { stack: StackItem }) => {
   return (
-    <div className="border border-neutral-500 rounded-lg p-4 flex items-center gap-3 min-w-[140px] sm:min-w-40 shadow-sm hover:scale-105 hover:border-neutral-50 transition-transform">
-      <div className="text-3xl text-neutral-100">{stack.icon}</div>
-      <div className="text-neutral-100 font-medium">{stack.name}</div>
+    <div className="border border-neutral-500 rounded-lg p-4 flex items-center gap-2 min-w-[140px] sm:min-w-40 shadow-sm hover:scale-105 hover:border-neutral-50 transition-transform">
+      <div className="text-2xl text-neutral-100">{stack.icon}</div>
+      <div className="text-neutral-100">{stack.name}</div>
     </div>
   );
 }
