03ff005d3f5e7595b9b119159672fd343a58d4c3
Kurasao <jamerolenor@gmail.com>
2025-11-05 08:41:21 +0800

    refactor: adjust text size for stack name in StackBox component for better readability

--- src/components/ui/StackBox.tsx
+++ src/components/ui/StackBox.tsx
@@ -7,7 +7,7 @@ const StackBox = ({ stack }: { stack: StackItem }) => {
   return (
     <div className="border border-neutral-500 rounded-lg p-4 flex items-center gap-2 min-w-[140px] sm:min-w-40 shadow-sm hover:scale-105 hover:border-neutral-50 transition-transform">
       <div className="text-2xl text-neutral-100">{stack.icon}</div>
-      <div className="text-neutral-100">{stack.name}</div>
+      <div className="text-neutral-100 text-sm">{stack.name}</div>
     </div>
   );
 }
