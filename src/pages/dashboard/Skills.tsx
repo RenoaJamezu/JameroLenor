5c73c6938a71fc016595f6c3fd4cf39f10e3783d
Kurasao <jamerolenor@gmail.com>
2025-12-28 16:33:54 +0800

    fix: adjust text size for technology icons in Skills component

--- src/pages/dashboard/Skills.tsx
+++ src/pages/dashboard/Skills.tsx
@@ -20,7 +20,7 @@ export default function Skills() {
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-3">
         {tech.map(({ tech, icon }) => (
-          <div key={tech} className="border rounded-xl text-gray-500 px-3 py-1 flex space-x-2 items-center justify-center text-xl transition-all duration-300 hover:text-white">
+          <div key={tech} className="border rounded-xl text-gray-500 px-3 py-1 flex space-x-2 items-center justify-center text-md md:text-xl transition-all duration-300 hover:text-white">
             <h1>{tech}</h1>
             {icon}
           </div>
