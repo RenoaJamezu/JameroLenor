eff9f25276aea87e2006f77047a884edffcad991
Kurasao <jamerolenor@gmail.com>
2026-01-09 22:52:18 +0800

    Refactor code structure for improved readability and maintainability

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -7,17 +7,17 @@ interface ImageModalProps {
 export default function ImageModal({ src, alt, onClose }: ImageModalProps) {
   return (
     <div 
-      className="fixed inset-0 bg-black/80 backdrop-blur-sm flex items-center justify-center z-60 font-mono"
+      className="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center justify-center z-60 font-mono p-4"
       onClick={onClose}
     >
       <div 
-        className="max-h-[90vh] max-w-[90vw] fade-zoom-in"
+        className="w-full max-w-2xl md:max-w-3xl fade-zoom-in p-5 bg-primary rounded-xl"
         onClick={(e) => e.stopPropagation()}
       >
         <img
           src={src}
           alt={alt}
-          className="w-full h-full object-contain rounded-lg"
+          className="w-full h-auto object-contain rounded-lg max-h-[75vh]"
         />
       </div>
     </div>
--- src/pages/dashboard/sections/Education.tsx
+++ src/pages/dashboard/sections/Education.tsx
@@ -1,24 +1,33 @@
 import { FaLink } from "react-icons/fa";
+import ImageModal from "../../../components/ImageModal";
+import { useState } from "react";
 
 export default function Education() {
+  const [enlargedImage, setEnlargedImage] = useState<string | null>(null);
   return (
     <section id="education" className="scroll-mt-20 px-4 md:px-50 py-20 md:py-30 flex flex-col bg-linear-to-br from-black to-gray-950 justify-center space-y-10 font-mono caret-transparent">
       <h1 className="text-xl md:text-2xl text-white">Education</h1>
-      <a
-        href="https://drive.google.com/file/d/1dVdhD9y5uFkHWUeJ0XwNNyYh_XVoOz3g/view?usp=sharing"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="flex flex-col w-fit bg-primary/50 p-5 rounded-xl text-white group">
+      <button
+        onClick={() => setEnlargedImage("/images/diploma.jpg")}
+        className="flex flex-col w-fit bg-primary/50 p-5 rounded-xl text-white group text-start">
         <div className="mt-1 flex gap-2 items-center group-hover:text-blue-800">
           <p className="text-xl font-medium">2021 - 2025</p>
-          <FaLink className="text-sm"/>
+          <FaLink className="text-sm" />
         </div>
         <div className="text-sm md:text-base">
           <p className="mt-1">Caraga State University - Main</p>
           <p>Bachelor of Science in Information Technology</p>
           <p>Graduated</p>
         </div>
-      </a>
+      </button>
+
+      {enlargedImage && (
+        <ImageModal
+          src={enlargedImage}
+          alt={enlargedImage}
+          onClose={() => setEnlargedImage(null)}
+        />
+      )}
     </section>
   )
 }
